ef93c2157309def86f9b6a2129128f479a350805
HoJLter <hojlter.work@gmail.com>
2025-05-21 21:20:38 +0300

    Add: settings in work v.1

---
+++ src/topseudo/config/settings.py
@@ -0,0 +1,17 @@
+import json
+
+config_structure = {
+    "API-key": "",
+    "localisation": "",
+    "pseudocode-type": ""
+}
+
+
+def create_config_file():
+    with open("settings.json", "w") as file:
+        pass
+
+
+
+
+create_config_file()
